193f320d873c62390791b77e78e3c2a2294aa194
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-04-17 18:42:22 +0000

    phaser

---
+++ js/game.js
@@ -0,0 +1,31 @@
+/* global phaser */
+
+// Copyright (c) 2024 Kai Nguyen All rights reserved
+//
+// Created by: Kai Nguyen
+// Created on: Apr 2024
+// This is the Phaser3 game configuration file
+
+/**
+ * Start Phaser Game
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // Background colour
+  backgroundColor: 0x5f6e7a
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
