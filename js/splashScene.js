1e989173b95156e66020d10fe35b8d0ba1a8fa23
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-04-26 17:49:55 +0000

    ...

--- js/splashScene.js
+++ js/splashScene.js
@@ -15,6 +15,8 @@ class SplashScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "splashScene"})
+
+    this.splashSceneBackgroundImage = null
   }
 
   /**
@@ -41,7 +43,13 @@ class SplashScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
    */
   create(data) {
-    // pass
+    this.splashSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "splashSceneBackground"
+    )
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
   }
 
   /**
@@ -51,7 +59,9 @@ class SplashScene extends Phaser.Scene {
    * @param {number} delta - The delta time in ms since the last frame
    */
   update(time, delta) {
-    this.scene.switch("titleScene")
+    if (time > 3000){
+      this.scene.switch("titleScene")
+    }
   }
 }
 
