ffc76e1f1af31b576d72f946b5da70f463e76dd3
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-05-01 17:55:35 +0000

    updated menuScene and created gameScene.js

--- js/game.js
+++ js/game.js
@@ -10,10 +10,12 @@
 import SplashScene from "./splashScene.js"
 import TitleScene from "./titleScene.js"
 import MenuScene from "./menuScene.js"
+import GameScene from "./gameScene.js"
 //create the new scenes
 const splashScene = new SplashScene()
 const titleScene = new TitleScene()
 const menuScene = new MenuScene()
+const gameScene = new GameScene()
 /**
  * Start Phaser Game
  */
@@ -43,5 +45,6 @@ const game = new Phaser.Game(config)
 game.scene.add("splashScene", splashScene)
 game.scene.add("titleScene", titleScene)
 game.scene.add("menuScene", menuScene)
+game.scene.add("gameScene", gameScene)
 // the start scene
 game.scene.start("splashScene")
